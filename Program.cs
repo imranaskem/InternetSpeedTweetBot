02d1b8a0c7ee904842136915da945ac8a8b8e940
imranaskem <imran.askem@gmail.com>
2016-12-15 15:45:40 +0000

    Basic bot working. Tweet returning null error to be found

--- Program.cs
+++ Program.cs
@@ -8,26 +8,95 @@ namespace InternetSpeedTweetBot
 {
     class Program
     {
+        static TimeSpan timeBetweenTests = new TimeSpan(0, 0, 30);
+
         static void Main(string[] args)
         {
+            var lastTimeCalc = DateTime.MinValue;
+
+            while (true)
+            {
+                var timeDiff = DateTime.UtcNow - lastTimeCalc;
+
+                if (timeDiff > timeBetweenTests)
+                {
+                    MeasureSpeed();
+                    lastTimeCalc = DateTime.UtcNow;
+                }
+            }
+        }
+
+        private static void MeasureSpeed()
+        {
             const string tempfile = "tempfile.tmp";
-            WebClient webClient = new WebClient();
+            const double threshold = 200;            
 
-            Console.WriteLine("Downloading file....");
+            Console.WriteLine("Downloading file...");
+            Console.WriteLine();
 
-            Stopwatch sw = Stopwatch.StartNew();
-            webClient.DownloadFile("http://dl.google.com/googletalk/googletalk-setup.exe", tempfile);
-            sw.Stop();
+            Stopwatch sw = new Stopwatch();
+
+            using (WebClient webClient = new WebClient())
+            {
+                sw.Start(); 
+                webClient.DownloadFile("https://dl.dropboxusercontent.com/u/2517348/testfile.tmp", tempfile);
+                sw.Stop();
+            }
 
             FileInfo fileInfo = new FileInfo(tempfile);
-            long speed = fileInfo.Length / sw.Elapsed.Seconds;
+            double fileSize = fileInfo.Length;
+            fileSize = Math.Round((fileSize / 1024 / 1024), 2);
+            var speed = Math.Round(fileSize / sw.Elapsed.TotalSeconds, 2);
 
-            Console.WriteLine("Download duration: {0}", sw.Elapsed);
-            Console.WriteLine("File size: {0}", fileInfo.Length.ToString("N0"));
-            Console.WriteLine("Speed: {0} bps ", speed.ToString("N0"));
+            Console.WriteLine($"Download duration: {sw.Elapsed}");
+            Console.WriteLine($"File size: {fileSize} MB");
+            Console.WriteLine($"Speed: {speed} MB/s ");
 
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadLine();
+            var internetSpeed = speed * 8;
+            Console.WriteLine($"Internet Speed: {internetSpeed} Mbps ");
+            Console.WriteLine();
+
+            if (internetSpeed < threshold)
+            {
+                Console.WriteLine("Current speed is below threshold, sending tweet...");
+                var tweetHappened = TweetISP(internetSpeed);
+
+                if (tweetHappened)
+                {
+                    Console.WriteLine("Tweet sent!");
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong, the tweet wasn't sent");
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Speed is above threshold");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Next test is in {timeBetweenTests.Minutes} minutes");
+        }
+
+        private static bool TweetISP(double speed)
+        {
+            var consumerKey = "7yVIvWbiabDBYn133iF5xiLeS";
+            var consumerSecretKey = "n0qLRdDLZrLZZQqBs6qLFSw9HOPQ0G8zhEDOFFw1vdaZriKq0A";
+            var accessToken = "809384501570838528-yhSq3uu7SL3oYQb3CeXSz2J1ebLgT7t";
+            var accessSecretToken = "YlsIUe49BtA80kUA07DWpqFkEMvUCdsP4vZXg5OBMNP09";
+
+            Auth.SetUserCredentials(consumerKey, consumerSecretKey, accessToken, accessSecretToken);
+
+            //var tweetMessage = $"@virginmedia My current download speed is {speed}Mbps but I'm paying for 200Mbps!";
+
+            var tweetMessage = "Test tweet";
+
+            ExceptionHandler.SwallowWebExceptions = false;
+
+            var tweet = Tweet.PublishTweet(tweetMessage)
+
+            return tweet.IsTweetPublished;
         }
     }
 }
