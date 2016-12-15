6d0f3cc8c5123f7de9f1c7e1b17d4c823ada094f
imranaskem <imran.askem@gmail.com>
2016-12-15 12:39:13 +0000

    Setup commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using Tweetinvi;
+
+namespace InternetSpeedTweetBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string tempfile = "tempfile.tmp";
+            WebClient webClient = new WebClient();
+
+            Console.WriteLine("Downloading file....");
+
+            Stopwatch sw = Stopwatch.StartNew();
+            webClient.DownloadFile("http://dl.google.com/googletalk/googletalk-setup.exe", tempfile);
+            sw.Stop();
+
+            FileInfo fileInfo = new FileInfo(tempfile);
+            long speed = fileInfo.Length / sw.Elapsed.Seconds;
+
+            Console.WriteLine("Download duration: {0}", sw.Elapsed);
+            Console.WriteLine("File size: {0}", fileInfo.Length.ToString("N0"));
+            Console.WriteLine("Speed: {0} bps ", speed.ToString("N0"));
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
